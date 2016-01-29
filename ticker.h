e537826970af90aad59f85ccd460910f5cd067ba
Derfador1 <derfador1@gmail.com>
2016-01-29 16:28:57 -0500

    added basic code and structures to header file

---
+++ ticker.h
@@ -0,0 +1,24 @@
+#ifndef TICKER_H
+	#define TICKER_H
+
+#include <stdbool.h>
+
+struct company {
+	char symbol[6];
+	size_t cents;
+	char *name;
+};
+
+struct tree {
+	struct company *data;
+	struct tree *left, *right;
+};
+
+typedef struct {
+	struct tree *root;
+	int (*cmp)(const struct company *, const struct company *);
+} market;
+
+bool tree_insert(struct tree *t, struct company *comp, int (*cmp)(const struct company *, const struct company *));
+struct company *stock_create(char *symbol, char *name, double price);
+
