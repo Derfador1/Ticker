f4834043c2b9a559f1575ffb84db366d96c2be29
Derfador1 <derfador1@gmail.com>
2016-02-01 15:06:47 -0500

    added header for tree copy function

--- ticker.h
+++ ticker.h
@@ -27,12 +27,13 @@ typedef struct {
 bool tree_insert(struct tree *t, struct company *comp, int (*cmp)(const struct company *a, const struct company *b));
 struct company *stock_create(char *symbol, char *name, double price);
 int compare_symbol(const struct company *a, const struct company *b);
-int cmp(const struct company *a, const struct company *b);
+int compare_cents(const struct company *a, const struct company *b);
 struct tree *tree_create(struct company *data);
 void tree_destroy(struct tree *t);
 market *market_create(int (*cmp)(const struct company *a, const struct company *b));
 market *market_insert(market *m, struct company *comp); 
 void market_destory(market *m);
 void tree_inorder(struct tree *t);
+void tree_copy(struct tree *dst, struct tree *src, int (*cmp)(const struct company *a, const struct company *b)
 
 #endif
